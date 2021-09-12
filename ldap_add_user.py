0cd1e2c54e082a54cb11d963f9d4a4a29589df47
xdz <1226032602@qq.com>
2021-09-12 11:08:24 +0800

    ldap_add_user

--- ldap_add_user.py
+++ ldap_add_user.py
@@ -8,12 +8,14 @@ ldap_user = 'cn=admin,dc=demo,dc=com'
 ldap_pwd = '123456'
 ldap_search_base = 'ou=jenkins,dc=demo,dc=com'
 
-# print(len(sys.argv))
 
-if len(sys.argv) != 3:
-    print('Usage:', sys.argv[0], 'user', 'ou')
+
+if len(sys.argv) < 3:
+    print('Usage:', sys.argv[0], 'user1', 'user2', '...', 'ou=demo,dc=demo,dc=com')
     quit(111)
-# uid = sys.argv[1]
-# ou = sys.argv[2]
-# l = ldapuser.LdapOps(ldap_server, ldap_user, ldap_pwd)
-# l.ldap_add_user(uid, ou)
+else:
+    ou = sys.argv[-1]
+    l = ldapuser.LdapOps(ldap_server, ldap_user, ldap_pwd)
+    for i in range(1, len(sys.argv) - 1):
+        l.ldap_add_user(sys.argv[i], ou)
+
