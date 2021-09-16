df97ac6073ad417cb1cafa42986dd426e6f8194e
xdz <1226032602@qq.com>
2021-09-16 13:44:28 +0800

    jenkins add role

---
+++ jenkins_add_role.py
@@ -0,0 +1,38 @@
+import myjenkins
+from myjenkins import jenkinsrole
+
+j = jenkinsrole.JenkinsRole('10.10.111.201', 'admin')
+j.host = "10.10.111.201"
+j.port = 8080
+j.username = "admin"
+j.token = '116c82f41b96860fd566d04ec7244d1965'
+
+
+role_type = 'projectRoles'
+permissions = 'com.cloudbees.plugins.credentials.CredentialsProvider.Update,hudson.model.Item.Create,hudson.model.Run.Delete,hudson.model.Item.Workspace,com.cloudbees.plugins.credentials.CredentialsProvider.Delete,com.cloudbees.plugins.credentials.CredentialsProvider.ManageDomains,hudson.model.Run.Replay,hudson.model.Item.Configure,org.jenkins.plugins.lockableresources.LockableResourcesManager.View,hudson.model.Item.Cancel,hudson.model.Item.Delete,hudson.model.Item.Read,com.cloudbees.plugins.credentials.CredentialsProvider.View,com.cloudbees.plugins.credentials.CredentialsProvider.Create,hudson.model.Item.Build,org.jenkins.plugins.lockableresources.LockableResourcesManager.Unlock,hudson.plugins.jobConfigHistory.JobConfigHistory.DeleteEntry,hudson.scm.SCM.Tag,hudson.model.Item.Move,org.jenkins.plugins.lockableresources.LockableResourcesManager.Reserve,hudson.model.Item.Discover,hudson.model.Run.Update'
+
+
+role_name_pattern = {
+'item_bpm':'.*bpm.*',
+'item_bucket':'.*bucket.*',
+'item_cp':'.*cp.*',
+'item_crm':'.*crm.*',
+'item_design':'.*design.*',
+'item_dms':'.*dms.*',
+'item_guimo':'.*guimo.*',
+'item_hrm':'.*hrm.*',
+'item_idm':'.*idm.*',
+'item_mdm':'.*mdm.*',
+'item_paas':'.*paas.*',
+'item_portal':'.*portal.*',
+'item_project':'.*project.*',
+'item_rest':'.*rest.*',
+'item_subscription':'.*subscription.*',
+'item_webrtc':'.*webrtc.*',
+'item_workflow':'.*workflow.*'
+}
+
+for role_name, role_pattern in role_name_pattern.items():
+    res_code = j.add_role(role_type, role_name, permissions, role_pattern)
+    print(res_code)
+
--- jenkins_user_grant.py
+++ jenkins_user_grant.py
@@ -21,11 +21,39 @@ def role_cancel_user(role_type, role_name, username):
     res = j.unassign_role(role_type, role_name, username)
     print('%s del %s %d' %(username, role_name, res))
 
-j = jenkinsrole.JenkinsRole('192.168.41.29', '5010905')
-j.host = "192.168.41.29"
+#new_pre_jenkins
+# j = jenkinsrole.JenkinsRole('192.168.41.29', '5010905')
+# j.host = "192.168.41.29"
+# j.port = 8080
+# j.username = "5010905"
+# j.token = '1130c5e4c3968bfe4c64edb9f0a8ecc5c5'
+
+
+#new_pre_jenkins
+j = jenkinsrole.JenkinsRole('10.10.111.201', 'admin')
+j.host = "10.10.111.201"
 j.port = 8080
-j.username = "5010905"
-j.token = '1130c5e4c3968bfe4c64edb9f0a8ecc5c5'
+j.username = "admin"
+j.token = '116c82f41b96860fd566d04ec7244d1965'
+
+
+global_role_type = 'globalRoles'
+global_role_name = 'base'
+
+role_type = 'projectRoles'
+# role_name = 'item_attachment'
+# role_pattern = '.*attachment.*'
+
+user = 'xdz10'
+
+# role_to_user(global_role_type, global_role_name, user)
+# role_to_user(role_type, role_name, user)
+# get_role_info(role_type, role_name)
+
+
+# for role_pattern in role_pattern_list:
+#     res_code = j.add_role(role_type, role_name, permissions, role_pattern)
+#     print(res_code)
 
 #new_pre ldap
 # ldap_server = '192.168.41.13'
@@ -35,21 +63,21 @@ j.token = '1130c5e4c3968bfe4c64edb9f0a8ecc5c5'
 
 
 #myvm ldap
-ldap_server = '10.10.111.159'
-ldap_user = 'cn=admin,dc=demo,dc=com'
-ldap_pwd = '123456'
+# ldap_server = '10.10.111.159'
+# ldap_user = 'cn=admin,dc=demo,dc=com'
+# ldap_pwd = '123456'
 # ldap_search_base = 'ou=jenkins,dc=demo,dc=com'
-ldap_search_base = 'ou=group1,dc=demo,dc=com'
+# ldap_search_base = 'ou=group1,dc=demo,dc=com'
 
-l = ldapuser.LdapOps(ldap_server, ldap_user, ldap_pwd)
+# l = ldapuser.LdapOps(ldap_server, ldap_user, ldap_pwd)
 # l.ldap_server = ldap_server
 # l.ldap_user = ldap_user
 # l.ldap_pwd = ldap_pwd
 
-uid_list = l.ldap_get_uid(ldap_search_base)
-print(uid_list)
+# uid_list = l.ldap_get_uid(ldap_search_base)
+# print(uid_list)
 
-uid = 'wuxingge2'
+# uid = 'wuxingge2'
 # ou = 'ou=group1,dc=demo,dc=com'
 # l.ldap_add_user(uid, ou)
 # l.ldap_del_user(uid, ou)
